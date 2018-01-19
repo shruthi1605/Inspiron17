f73eedf0c4cc042cc9a55d502904be96c0a7003e
shruthi1605 <iamshruthi.rk@gmail.com>
2018-01-19 22:40:54 +0530

    updated js

---
+++ js/svg-animation.js
@@ -0,0 +1,41 @@
+var demo = (function (window) {
+    
+    'use strict';
+        
+    var SELECTORS = {
+            textRows: '.row'
+        },
+        
+        CLASSES = {
+            drawingWord: 'drawing'
+        },
+        
+        textRows = document.querySelectorAll(SELECTORS.textRows);
+    
+        
+    
+    function applyDrawEffect (rowSVG) {        
+      rowSVG.classList.add(CLASSES.drawingWord);            
+    }
+        
+    
+    function drawWords () {        
+        [].forEach.call(textRows, applyDrawEffect);        
+    }
+    
+    
+    function init () {
+        drawWords();    
+    }
+    
+    
+    return {
+        init: init
+    };
+    
+}(window));
+
+
+
+
+window.addEventListener('DOMContentLoaded', demo.init, false);
